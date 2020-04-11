df87329326cc34e6f4e32da7652f60ecc461ea43
Samuel Sandri <sjs234@uclive.ac.nz>
2020-04-11 18:22:50 +1200

    Tried changing middleware to fix EPIPE error

--- config/express.js
+++ config/express.js
@@ -10,9 +10,9 @@ module.exports = function () {
 
     // MIDDLEWARE
     app.use(allowCrossOriginRequestsMiddleware);
+    app.use(bodyParser.raw({ type: [ 'image/*' ], limit: '20mb' }));
     app.use(bodyParser.json());
     app.use(bodyParser.raw({ type: 'text/plain' }));  // for the /executeSql endpoint
-    app.use(bodyParser.raw({ type: [ 'image/jpeg', 'image/gif', 'image/png' ], limit: '20mb' }));
 
     // DEBUG (you can remove these)
     app.use((req, res, next) => {
